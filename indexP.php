d9db5a19361bdff81606dd9342c4dea445fea41f
rebeccawir23 <42651632+rebeccawir23@users.noreply.github.com>
2019-04-11 09:40:04 -0600

    Add files via upload

---
+++ indexP.php
@@ -0,0 +1,53 @@
+<
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<link rel = "stylesheetP"
+   type = "text/css"
+   href = "stylesheetP.css" />
+	<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+</head>
+<div class="topnav">
+  	<a class="active" href="indexP.php">Home</a>
+    
+</div>
+
+	
+	
+<center>  
+  <h1>Rebecca Wirick</h1>
+  
+</center>
+</html>
+<html>
+
+<body>
+	
+<div class="container">
+  <div class="picgrid">
+	 <img class="picture" src="IMPACT/image9.jpg" width = 100%>
+	</div>
+  <h3 id="rTitle">Rebecca Wirick</h3>
+	  <center><img src="Images/Wirick.JPG" class="bioPic"></center><br>
+    My name is Rebecca Wirick. I am in my fourth year at Boise State and have one more year after this one. 
+    I changed my major to GIMM last year from computer science and have been enjoying it ever since. 
+    I was born and raised outside of Ft. Worth, Texas and moved to Boise to come to school. On campus I am involved in 
+    The student IMPACT Board as the VP of Marketing so I plan on using the skills I learn this semester in WebDev to create a website for 
+the organization. I currently work at The Discovery Center of Idaho and really like the opportunity that 
+I have through my job get kids excited about STEM opportunities. Outside of school and work, I love to ski, hike, and watch football 
+with friends. I am unsure about my plans after graduation, but I think developing the technology we study into educational use, 
+whether it be in classrooms or training simulations is something I am interested in. </div>
+</body>
+</html>
+
+</center></div>
+
+	</div>
+ 
+</div>
+	
+</body>
+</html>
